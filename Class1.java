b6af0e71ea7e048fc479b634d9bf11c498615e1e
mzuritacortes24 <mzuritacortes@stu.jsu.edu>
2024-01-19 13:46:29 -0600

    Initial Commit

---
+++ Class1.java
@@ -0,0 +1,7 @@
+public class Class1   {
+    
+    private void f1() {
+        // Placeholder method (v1.0)
+    }
+        
+}
---
+++ Class2.java
@@ -0,0 +1,7 @@
+public class Class2   {
+
+    private void f1() {
+        // Placeholder method (v1.0)
+    }        
+    
+}
